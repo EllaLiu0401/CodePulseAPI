d5202b180d7a7f89470ff1037108bb72d362c421
EllaLiu0401 <ellatong0515@gmail.com>
2024-10-27 20:44:44 +1100

    feat(models): add BlogPost and Category classes

--- CodePulse.API/obj/Debug/net8.0/CodePulse.API.AssemblyInfo.cs
+++ CodePulse.API/obj/Debug/net8.0/CodePulse.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CodePulse.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1d4793265761beed9dfd7ec670a76de3707d11fd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+53aee448ec7f57c9a1ec9737a84db4118cabf287")]
 [assembly: System.Reflection.AssemblyProductAttribute("CodePulse.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CodePulse.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
