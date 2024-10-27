05ad0263a236362a0b2f57d03022393ea60f6988
EllaLiu0401 <ellatong0515@gmail.com>
2024-10-27 23:24:55 +1100

    chore(db): configure database connection in appsettings.json and Program.cs

--- CodePulse.API/Program.cs
+++ CodePulse.API/Program.cs
@@ -1,9 +1,17 @@
+using CodePulse.API.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Inject DbContext
+builder.Services.AddDbContext<ApplicationDbContext>(options => {
+    options.UseSqlServer(builder.Configuration.GetConnectionString("CodePulseConnectionString"));
+});
 
 var app = builder.Build();
 
